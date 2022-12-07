086cd194c32f9167f8bc1cb279cf29e43dd1592b
Kamlesh Shrestha <koderlad@gmail.com>
2022-12-07 17:21:24 +0545

    Created Child Menus for parent and child pages, using the parent-child relation.

--- page.php
+++ page.php
@@ -15,19 +15,30 @@
     </div>
 
     <div class="container container--narrow page-section">
-      <div class="metabox metabox--position-up metabox--with-home-link">
-        <p>
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
-        </p>
-      </div>
 
-      <!-- <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
+    <?   
+      $theParent = wp_get_post_parent_id(get_the_ID());
+      if($theParent): ?>
+        <div class="metabox metabox--position-up metabox--with-home-link">
+          <p>
+            <a class="metabox__blog-home-link" href="<? echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <? echo get_the_title($theParent); ?></a> <span class="metabox__main"><? the_title(); ?></span>
+          </p>
+        </div>
+      <? endif; ?>
+      
+
+      <div class="page-links">
+        <h2 class="page-links__title"><a href="#"><? echo get_the_title($theParent); ?></a></h2>
         <ul class="min-list">
-          <li class="current_page_item"><a href="#">Our History</a></li>
-          <li><a href="#">Our Goals</a></li>
+          <? 
+
+            wp_list_pages(array(
+              'title_li' => NULL,
+              'child_of' => $theParent ? $theParent : get_the_ID()
+            ));
+          ?>
         </ul>
-      </div> -->
+      </div>
 
       <div class="generic-content">
       <? the_content(); ?>  
